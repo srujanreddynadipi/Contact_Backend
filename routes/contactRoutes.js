a2d213c617517e4afb8751e8ab0b8229f3de7353
classroom <srujanreddynsreddy@gmail.com>
2025-04-18 18:10:17 +0530

    this is the node js backend code

---
+++ routes/contactRoutes.js
@@ -0,0 +1,15 @@
+const express = require('express');  // importing express
+const router = express.Router();  // making an object of express into router
+const {getContacts,getContact, createContact, updateContact, deleteContact} = require('../controllers/contactController');  // importing the getContact function from contactController
+
+router.route('/').get(getContacts);  // this is the middleware for the server
+
+router.route('/').post(createContact);  
+
+router.route('/:id').get(getContact); 
+
+router.route('/:id').put(updateContact);  
+
+router.route('/:id').delete(deleteContact);  
+
+module.exports = router;  // exporting the router object to be used in other files
---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+
+const app = express();  // making an object of express into app
+
+const port = process.env.PORT || 5000;  // this used to run the server on port 5001 or 5000
+
+app.use(express.json());  // this is used to parse the json data in the request body
+app.use('/api/contacts', require('./routes/contactRoutes'));  // this is the middleware for the server
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`); // running the server on the port 
+});
+
