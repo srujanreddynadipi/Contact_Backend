63563208935693c99e344226e78873ca930d8c67
classroom <srujanreddynsreddy@gmail.com>
2025-04-18 18:10:17 +0530

    this is the node js backend code

---
+++ controllers/contactController.js
@@ -0,0 +1,43 @@
+//@desc Get all contacts
+//@route GET /api/contacts
+//@access public
+
+const getContacts = (req, res) => {
+    res.status(200).json({ message: 'get all the contacts  ' });
+};
+//@desc Create new  contacts
+//@route POST /api/contacts
+//@access public
+
+const createContact = (req, res) => {
+    console.log("the request body is ", req.body); // this will log the request body in the console
+    res.status(201).json({ message: 'Create  contacts  ' });
+};
+
+
+//@desc Get contacts
+//@route GET /api/contacts/:id
+//@access public
+
+const getContact = (req, res) => {
+    res.status(200).json({ message: `Get contact for ${req.params.id} ` });
+};
+
+//@desc Update contacts
+//@route PUT /api/contacts/:id
+//@access public
+
+const updateContact = (req, res) => {
+    res.status(200).json({ message: `Update contact for ${req.params.id} ` });
+};
+//@desc Delete contacts
+//@route Delete /api/contacts/:id
+//@access public
+
+const deleteContact = (req, res) => {
+    res.status(200).json({ message: `Delete contact for ${req.params.id} ` });
+};
+
+
+
+module.exports = { getContacts, createContact, getContact, updateContact, deleteContact }; // exporting the getContact and createContact functions to be used in other files
